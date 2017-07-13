3734965148a6c5adb94736d950735b0ead4fecee
heizen13 <julzz.depaz@gmail.com>
2017-07-13 09:00:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ bot.dialog('/', function (session) {
       }
   
    else if(session.message.text.toLowerCase().contains('dubidubidap')){
-      session.send(`Beep beep beep.. and sabi ng jeep.. beep beep beep beep bee-beep beep!!!`);
+      session.send(`/me Beep beep beep.. and sabi ng jeep.. beep beep beep beep bee-beep beep!!!`);
       }
    
    else{
