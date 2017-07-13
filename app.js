48742b20e82ff75e41d7e2d4da8bef1197668ad2
heizen13 <julzz.depaz@gmail.com>
2017-07-13 08:37:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ bot.dialog('/', function (session) {
     if(session.message.text.toLowerCase().contains('hello')){
       session.send(`Hello bruh!, Zup bruh? Howzi goin bruh?!`);
       }else if(session.message.text.toLowerCase().contains('help')){
-        session.send(`How can I help you bruh? I don't have any money bruh!? but I can give you a hug..... :$ `);
+        session.send(`How can I help you bruh? I don't have any money... b-but I can give you a hug..... :$ `);
           }
    else if(session.message.text.toLowerCase().contains('who are you?')){
         session.send("Hello, I'm bowtt and I'm a cute bot. Some people tells me that I look like a balls and a butt.");
