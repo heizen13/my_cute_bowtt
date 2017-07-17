56b60c7682de7f17b9d1205d09584e6d7b0c82e9
heizen13 <julzz.depaz@gmail.com>
2017-07-17 13:57:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -202,22 +202,7 @@ else if(session.message.text.toLowerCase().contains('say ')){
    
    
    
-   
-      //youtube
-   
-else if(session.message.text.toLowerCase().contains('play kuya jobert')){
-      
-      var reply = context.MakeMessage(); 
-   reply.Text = "https://youtu.be/I2A-vZ-jLDo"; 
-   await context.PostAsync(reply); 
-   
 
-}
-   
-//youtube   
-   
-   
-   
 
    
    else{
