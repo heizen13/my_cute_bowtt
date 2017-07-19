d3ac4c68edb63e01445f2271b698a801faa91004
heizen13 <julzz.depaz@gmail.com>
2017-07-19 16:09:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -322,7 +322,7 @@ session.send('notbad, herewego, teachme, bitch plz, ifyouknow, no no, gaay');
          
       session.send('👂--👀');
       session.send(' . . . . 👃');
-      session.send(str.replace(/'/g, "\\'"));
+      session.send(str.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&"));
      
    }
 
